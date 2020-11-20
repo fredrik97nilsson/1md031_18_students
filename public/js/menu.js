3fe9f5fd3ff9d15d792b67de50dd83f81be4a81f
Fredrik <fredrik97nilsson@gmail.com>
2020-11-20 12:21:26 +0100

    adderar menyn

---
+++ public/js/menu.js
@@ -0,0 +1,74 @@
+var food = [
+  {
+    "name": "Orginalburgare",
+    "kCal": 300,
+    "lactos": false,
+    "gluten": true,
+    "vegetarisk":false,
+    "chosen": 0,
+    "filtrerad":true,
+    "img": "https://pagen.se/globalassets/kampanjer/garden-party-2018/burgare-med-vitloks--och-chilimajonas.png.png?w=734&h=462&mode=crop&resized=true"
+  },
+  {
+    "name": "Specialaren",
+    "kCal": 300,
+    "lactos": false,
+    "gluten": true,
+    "vegetarisk":false,
+    "chosen": 0,
+    "filtrerad":true,
+    "img": "https://cdn1.hemtrevligt.se/YTo2OntzOjI6ImlkIjtpOjE1MzYyOTQ7czoxOiJ3IjtpOjEwMjA7czoxOiJoIjtpOjMyMDA7czoxOiJjIjtpOjA7czoxOiJzIjtpOjA7czoxOiJrIjtzOjQwOiI3ZWU0OTBlODQ1MTk4ZDk2M2NjZmUwZTQ2MTMxMjY0NjhkYTI5NmY5Ijt9"
+  },
+  {
+  "name":"HjärtATTACK" ,
+  "kCal": 2500 ,
+  "lactos":true ,
+  "gluten":true ,
+  "vegetarisk":false ,
+  "chosen": 0,
+  "filtrerad":true,
+  "img": "https://cdn-rdb.arla.com/Files/arla-se/2409428905/58cad7a0-bc79-45cb-9f9f-07591fe5b22c.jpg?mode=crop&w=479&h=335&ak=f525e733&hm=d7d1b1dd"
+},
+{
+"name": "Liten",
+"kCal": 150,
+"lactos":true ,
+"gluten":false ,
+"vegetarisk": false,
+"chosen": 0,
+"filtrerad":true,
+"img": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGkgnmNcZNHcJi28VCXOYhbSB7-wqedBA07A&usqp=CAU"
+},
+{
+"name":"Tråkig" ,
+"kCal": "300",
+"lactos": false,
+"gluten": false,
+"vegetarisk": true,
+"chosen": 0,
+"filtrerad":true,
+"img": "https://www.preem.se/globalassets/privat/i-butiken/hamburgare/hamburgare_2019.png?preset=StandardPageImage"
+},
+{
+"name":"Den funkar" ,
+"kCal":500 ,
+"lactos": false,
+"gluten": true,
+"vegetarisk": false,
+"chosen": 0,
+"filtrerad":true,
+"img": "https://static.feber.se/article_images/29/99/57/299957.jpg"
+},
+{
+"name":"'En till'" ,
+"kCal":800 ,
+"lactos": true,
+"gluten":true ,
+"vegetarisk":true ,
+"chosen": 0,
+"filtrerad":true,
+"img": "https://www.ica.se//assets.icanet.se/q_auto,f_auto/imagevaultfiles/id_62974/cf_6901/hamburgare_deluxe_med_rostad_f-rskpotatis-715373.jpg"
+}
+
+
+]
